cebe9c6d49e68a05538c443d3f691708a512409f
nueladeyemi1 <emmanuel.adeyemi40@gmail.com>
2023-04-16 23:55:36 +0100

    mod: styled

--- react-hero-section/src/App.jsx
+++ react-hero-section/src/App.jsx
@@ -1,35 +1,49 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+    <>
+      <header>
+        <div className="container">
+          <nav>
+            <div>LOGO</div>
+            <div>NAVIGATION</div>
+          </nav>
+        </div>
+        <div className="header__container">
+          <div className="header__inner__container">
+            <h1>A healty meal delivered to your door, every single day</h1>
+            <p>
+              The smart 365-days-per-year food subscription that will make you
+              eat healty algain. Tailored to your personal tastes and
+              nutritional needs
+            </p>
+            <a className="btn" href="#">
+              Start eating well
+            </a>
+          </div>
+        </div>
+      </header>
+
+      <section>
+        <div className="container">
+          <h2>Some random heading</h2>
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dicta
+            quis, dolorum voluptas rem consectetur iusto? Esse similique hic,
+            laboriosam minima non veniam deleniti quibusdam eius maiores, illum,
+            quaerat blanditiis nemo? Lorem ipsum dolor sit amet consectetur
+            adipisicing elit. Quo, omnis eum? Ipsa quam dolore nulla ipsum at
+            delectus ipsam sequi esse. Similique facilis, unde hic molestias
+            amet quos accusantium eos.
+          </p>
+        </div>
+      </section>
+    </>
+  );
 }
 
 export default App
