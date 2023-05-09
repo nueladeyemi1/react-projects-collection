cc28ee9fd7c744d45fdb85396d16223cb924463a
nueladeyemi1 <emmanuel.adeyemi40@gmail.com>
2023-05-09 06:05:08 +0100

    hero-part

--- Omnifood-react/src/App.jsx
+++ Omnifood-react/src/App.jsx
@@ -2,11 +2,12 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Hero from './Hero'
 
 function App() {
   return (
     <>
-      <h1>A healthy meal delivered to your door, every single day</h1>
+      <Hero />
     </>
   )
 }
---
+++ Omnifood-react/src/Hero.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import heroImage from './img/hero.png'
+
+const Hero = () => {
+  return (
+    <section className='hero-section'>
+      <div className='hero'>
+        <div className='hero-text-box'>
+          <h1 className='primary-title'>
+            A healthy meal delivered to your door, every single day
+          </h1>
+          <p className='hero-text'>
+            The smart 365-days-per-year food subscription that will make you eat
+            healthy again. Tailored to your personal tastes and nutritional
+            needs.
+          </p>
+          <a href='#' className='btn btn-start btn-margin'>
+            Start eating well
+          </a>
+          <a href='#' className='btn btn-see'>
+            See More &darr;
+          </a>
+        </div>
+        <div className='hero-img-box'>
+          <img
+            className='hero-img'
+            src={heroImage}
+            alt='Hero Image of woman eating'
+          />
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default Hero
