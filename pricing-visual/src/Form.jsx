7e0593753ed2987f71ac25eb8e6b0cfdb49f2b41
nueladeyemi1 <emmanuel.adeyemi40@gmail.com>
2023-05-21 22:04:24 +0100

    handle button

--- pricing-visual/src/Form.jsx
+++ pricing-visual/src/Form.jsx
@@ -9,9 +9,12 @@ import {
   selectData1,
 } from './formdata'
 import axios from 'axios'
+import { Fade } from 'react-reveal'
 
 const Form = () => {
   const [checkboxState, setCheckboxState] = useState(true)
+  const [show, setShow] = useState(false)
+  const [hideNext, setHideNext] = useState(true)
   const [data, setData] = useState({
     firstName: '',
     lastName: '',
@@ -39,10 +42,23 @@ const Form = () => {
     contacting: [],
   })
 
+  const listArray = []
+
   function handle(e) {
     const newData = { ...data }
     newData[e.target.id] = e.target.value
 
+    console.log(newData.contacting)
+
+    const thecheckArray = document.querySelectorAll('.thecheckbox')
+    console.log(thecheckArray)
+    for (let checkbox of thecheckArray) {
+      if (checkbox.checked === true) {
+        let val = listArray.push(checkbox.value)
+        val = newData[contacting]
+        console.log(listArray, newData[contacting])
+      }
+    }
     // if ('checked' in e.target) {
     //   this.setState({ [e.target.name]: e.target.checked })
     // }
@@ -55,8 +71,13 @@ const Form = () => {
     //   newData[e.target.id].push(e.target.value)
     // }
     setData(newData)
-    console.log(e.target.id, e.target.checked, newData[e.target.id])
+    // console.log(e.target.id, e.target.checked, newData[e.target.id])
     console.log(newData)
+  }
+
+  const handleNext = function() {
+    setShow(!show)
+    setHideNext(false)
   }
 
   const postRequest = function(e) {
@@ -137,88 +158,103 @@ const Form = () => {
                 </div>
               )
             })}
+            {hideNext && (
+              <button onClick={() => handleNext()} className='btn-hero next'>
+                Next
+              </button>
+            )}
           </div>
 
           {/* // */}
 
           {/* <section className='section-form--2'> */}
-          <div>
-            <h2 className='second-form--1'>
-              Tell us a little more about yourself professionally.
-            </h2>
-            <p className='second-form-text--1'>
-              Don't overthink your answers! This is just a demo.
-            </p>
-            {textAreaData.map(({ id, name, title, sample, placeHolder }) => {
-              return (
-                <label className='textarea-label'>
-                  <p className='text-title'>{title}</p>
-                  <span>{sample}</span>
-                  <textarea
-                    className='text-area-form'
-                    onChange={(e) => handle(e)}
-                    type='text'
-                    id={name}
-                    value={data.name}
-                    //   value=''
-                    placeholder={placeHolder}
-                    required
-                  ></textarea>
-                </label>
-              )
-            })}
-          </div>
-          <div>
-            <h2 className='second-form--2'>
-              Tell us about your biz dev operations
-            </h2>
-            <p className='second-form-text--2'>
-              This doesn't need to be exact. We'll use this information to
-              create your business case.
-            </p>
-            {inputData.map(({ id, name, title, sample, placeHolder }) => {
-              return (
-                <label className='textarea-label'>
-                  <p className='text-title'>{title}</p>
-                  <span>{sample}</span>
-                  <input
-                    className='text-area-form'
-                    onChange={(e) => handle(e)}
-                    type='number'
-                    id={name}
-                    value={data.name}
-                    placeholder={placeHolder}
-                    required
-                  />
-                </label>
-              )
-            })}
-            {selectData1.map(({ id, name1, question, options }) => {
-              return (
-                <>
-                  <p className='text-title title-check'>{question}</p>
-                  <div className='checkbox'>
-                    {options.map((opt) => (
-                      <div>
-                        <input
-                          className='thecheckbox'
+
+          {show && (
+            <Fade Top>
+              <div className='opacity1'>
+                <h2 className='second-form--1'>
+                  Tell us a little more about yourself professionally.
+                </h2>
+                <p className='second-form-text--1'>
+                  Don't overthink your answers! This is just a demo.
+                </p>
+                {textAreaData.map(
+                  ({ id, name, title, sample, placeHolder }) => {
+                    return (
+                      <label key={id} className='textarea-label'>
+                        <p className='text-title'>{title}</p>
+                        <span>{sample}</span>
+                        <textarea
+                          className='text-area-form'
                           onChange={(e) => handle(e)}
-                          id={name1}
-                          value={opt}
-                          type='checkbox'
+                          type='text'
+                          id={name}
+                          value={data.name}
+                          //   value=''
+                          placeholder={placeHolder}
                           required
-                        />
-                        <label className='checkbox-label'>{opt}</label>
-                      </div>
-                    ))}
-                  </div>
-                </>
-              )
-            })}
-          </div>
+                        ></textarea>
+                      </label>
+                    )
+                  }
+                )}
+              </div>
+            </Fade>
+          )}
+          {show && (
+            <div className='opacity1'>
+              <h2 className='second-form--2'>
+                Tell us about your biz dev operations
+              </h2>
+              <p className='second-form-text--2'>
+                This doesn't need to be exact. We'll use this information to
+                create your business case.
+              </p>
+              {inputData.map(({ id, name, title, sample, placeHolder }) => {
+                return (
+                  <label key={id} className='textarea-label'>
+                    <p className='text-title'>{title}</p>
+                    <span>{sample}</span>
+                    <input
+                      className='text-area-form'
+                      onChange={(e) => handle(e)}
+                      type='number'
+                      id={name}
+                      value={data.name}
+                      placeholder={placeHolder}
+                      required
+                    />
+                  </label>
+                )
+              })}
+              {selectData1.map(({ id, name1, question, options }) => {
+                return (
+                  <>
+                    <p className='text-title title-check'>{question}</p>
+                    <div key={id} className='checkbox'>
+                      {options.map((opt) => (
+                        <div>
+                          <input
+                            className='thecheckbox'
+                            onChange={(e) => handle(e)}
+                            id={name1}
+                            value={opt}
+                            type='checkbox'
+                            required
+                          />
+                          <label className='checkbox-label'>{opt}</label>
+                        </div>
+                      ))}
+                    </div>
+                  </>
+                )
+              })}
+            </div>
+          )}
+
           {/* </section> */}
         </div>
-        <button className='btn-hero'>Submit</button>
+        {show && <button className='btn-hero opacity1'>Submit</button>}
       </form>
     </section>
   )
