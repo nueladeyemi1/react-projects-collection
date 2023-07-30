0472a03eb5a1ceb8ff5bb0319f041f487d4a1a21
nueladeyemi1 <emmanuel.adeyemi40@gmail.com>
2023-07-30 20:17:30 +0100

    handle API

--- weather-forecast-react/weather-forecast/src/App.jsx
+++ weather-forecast-react/weather-forecast/src/App.jsx
@@ -1,33 +1,94 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect } from 'react'
 import './App.css'
 
+function getWeatherIcon(wmoCode) {
+  const icons = new Map([
+    [[0], '☀️'],
+    [[1], '🌤'],
+    [[2], '⛅️'],
+    [[3], '☁️'],
+    [[45, 48], '🌫'],
+    [[51, 56, 61, 66, 80], '🌦'],
+    [[53, 55, 63, 65, 57, 67, 81, 82], '🌧'],
+    [[71, 73, 75, 77, 85, 86], '🌨'],
+    [[95], '🌩'],
+    [[96, 99], '⛈'],
+  ])
+  const arr = [...icons.keys()].find((key) => key.includes(wmoCode))
+  if (!arr) return 'NOT FOUND'
+  return icons.get(arr)
+}
+
 function App() {
-  const [count, setCount] = useState(0)
+  const [inputValue, setInputValue] = useState('Nigeria')
+  const [data, setData] = useState({})
+
+  function convertToFlag(countryCode) {
+    const codePoints = countryCode
+      .toUpperCase()
+      .split('')
+      .map((char) => 127397 + char.charCodeAt())
+    return String.fromCodePoint(...codePoints)
+  }
+
+  function formatDay(dateStr) {
+    return new Intl.DateTimeFormat('en', {
+      weekday: 'short',
+    }).format(new Date(dateStr))
+  }
+
+  useEffect(
+    function() {
+      async function getWeather(location) {
+        try {
+          // 1) Getting location (geocoding)
+          const geoRes = await fetch(
+            `https://geocoding-api.open-meteo.com/v1/search?name=${location}`
+          )
+          const geoData = await geoRes.json()
+          console.log(geoData)
+
+          if (!geoData.results) throw new Error('Location not found')
+
+          const {
+            latitude,
+            longitude,
+            timezone,
+            name,
+            country_code,
+          } = geoData.results.at(0)
+          console.log(`${name} ${convertToFlag(country_code)}`)
+
+          // 2) Getting actual weather
+          const weatherRes = await fetch(
+            `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&timezone=${timezone}&daily=weathercode,temperature_2m_max,temperature_2m_min`
+          )
+          const weatherData = await weatherRes.json()
+          setData(weatherData.daily)
+        } catch (err) {
+          console.err(err)
+        }
+      }
+      getWeather(inputValue)
+    },
+    [inputValue]
+  )
+
+  const {
+    temperature_2m_max: maximumTemp,
+    temperature_2m_min: minimumTemp,
+    time,
+    weathercode,
+  } = data
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <input
+        value={inputValue}
+        onChange={(e) => setInputValue(e.target.value)}
+        placeholder='Enter the area'
+      />
     </>
   )
 }
