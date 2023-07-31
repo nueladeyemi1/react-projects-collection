1b58b6b6f29bcf636859e43679724d84dbbe8892
nueladeyemi1 <emmanuel.adeyemi40@gmail.com>
2023-07-31 20:52:30 +0100

    handling UI

--- weather-forecast-react/weather-forecast/src/App.jsx
+++ weather-forecast-react/weather-forecast/src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import { useEffect } from 'react'
 import './App.css'
+import { Weather } from './Weather'
 
 function getWeatherIcon(wmoCode) {
   const icons = new Map([
@@ -16,19 +17,25 @@ function getWeatherIcon(wmoCode) {
     [[96, 99], '⛈'],
   ])
   const arr = [...icons.keys()].find((key) => key.includes(wmoCode))
+  console.log(arr)
   if (!arr) return 'NOT FOUND'
   return icons.get(arr)
 }
 
 function App() {
+  const [isLoading, setIsLoading] = useState(false)
   const [inputValue, setInputValue] = useState('Nigeria')
   const [data, setData] = useState({})
+  const [countryCode, setCountryCode] = useState('')
+  const [displayLocation, setDisplayLocation] = useState({})
 
   function convertToFlag(countryCode) {
     const codePoints = countryCode
       .toUpperCase()
       .split('')
       .map((char) => 127397 + char.charCodeAt())
+
+    console.log(codePoints)
     return String.fromCodePoint(...codePoints)
   }
 
@@ -40,6 +47,7 @@ function App() {
 
   useEffect(
     function() {
+      setIsLoading(true)
       async function getWeather(location) {
         try {
           // 1) Getting location (geocoding)
@@ -58,7 +66,11 @@ function App() {
             name,
             country_code,
           } = geoData.results.at(0)
-          console.log(`${name} ${convertToFlag(country_code)}`)
+          setCountryCode(country_code)
+
+          setDisplayLocation({
+            displayLocation: `${name} ${convertToFlag(country_code)}`,
+          })
 
           // 2) Getting actual weather
           const weatherRes = await fetch(
@@ -68,6 +80,8 @@ function App() {
           setData(weatherData.daily)
         } catch (err) {
           console.err(err)
+        } finally {
+          setIsLoading(false)
         }
       }
       getWeather(inputValue)
@@ -82,14 +96,25 @@ function App() {
     weathercode,
   } = data
 
+  getWeatherIcon(weathercode)
+  convertToFlag(countryCode)
+  console.log(weathercode)
+
+  console.log(data)
+
   return (
-    <>
-      <input
-        value={inputValue}
-        onChange={(e) => setInputValue(e.target.value)}
-        placeholder='Enter the area'
-      />
-    </>
+    <div className='app'>
+      <h1>Classy Weather</h1>
+      <div>
+        <input
+          value={inputValue}
+          onChange={(e) => setInputValue(e.target.value)}
+          placeholder='Enter the area'
+        />
+      </div>
+      {isLoading && <p>Loading...</p>}
+      {weathercode && <Weather />}
+    </div>
   )
 }
 
