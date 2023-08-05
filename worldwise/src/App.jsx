382a3641c67bd6a4303bd4c051104901601f9447
nueladeyemi1 <emmanuel.adeyemi40@gmail.com>
2023-08-05 13:49:54 +0100

    handle routes

--- worldwise/src/App.jsx
+++ worldwise/src/App.jsx
@@ -1,7 +1,28 @@
-const App = () => {
-  const x = 23
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Homepage from './pages/Homepage'
+import Product from './pages/Product'
+import Pricing from './pages/Pricing'
+import PageNotFound from './pages/PageNotFound'
+import Login from './pages/Login'
+import AppLayout from './pages/AppLayout'
 
-  return <div>Worldwise</div>
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Homepage />} />
+        <Route path='product' element={<Product />} />
+        <Route path='pricing' element={<Pricing />} />
+        <Route path='login' element={<Login />} />
+        <Route path='app' element={<AppLayout />}>
+          <Route index element={<p>Mini Home</p>} />
+          <Route path='cities' element={<p>Cities</p>} />
+          <Route path='countries' element={<p>Countires</p>} />
+        </Route>
+        <Route path='*' element={<PageNotFound />} />
+      </Routes>
+    </BrowserRouter>
+  )
 }
 
 export default App
