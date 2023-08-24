e73f71a85e8d1b308adaa4308ef38e4a96ecb767
nueladeyemi1 <emmanuel.adeyemi40@gmail.com>
2023-08-24 12:23:06 +0100

    supabase

---
+++ the-wild-oasis/src/services/supabase.js
@@ -0,0 +1,8 @@
+import { createClient } from '@supabase/supabase-js'
+
+const supabaseUrl = 'https://sbrxjiyymyvkirnsbgob.supabase.co'
+const supabaseKey =
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNicnhqaXl5bXl2a2lybnNiZ29iIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTI3ODQ2NjUsImV4cCI6MjAwODM2MDY2NX0.QBV9NHhYNcDp3UiVz9ZfVfR7hifcFfwaD3tRdnbNrCw'
+const supabase = createClient(supabaseUrl, supabaseKey)
+
+export default supabase
