9f59e4249a0bb9d1e506ce2767dee6baf0f12896
nueladeyemi1 <emmanuel.adeyemi40@gmail.com>
2023-09-18 21:29:07 +0100

    dark mode

---
+++ react-wild-oasis/src/context/DarkModeContext.jsx
@@ -0,0 +1,44 @@
+import { useContext } from 'react'
+import { useEffect } from 'react'
+import { createContext } from 'react'
+import { useLocalStorageState } from '../hooks/useLocalStorageState'
+
+const DarkModeContext = createContext()
+
+const DarkModeProvider = ({ children }) => {
+  const [isDarkMode, setIsDarkMode] = useLocalStorageState(false, 'isDarkMode')
+
+  function toggleDarkMode() {
+    setIsDarkMode((isDark) => !isDark)
+  }
+
+  useEffect(
+    function() {
+      if (isDarkMode) {
+        document.documentElement.classList.add('dark-mode')
+        document.documentElement.classList.remove('light-mode')
+      } else {
+        document.documentElement.classList.add('light-mode')
+        document.documentElement.classList.remove('dark-mode')
+      }
+    },
+    [isDarkMode]
+  )
+
+  return (
+    <DarkModeContext.Provider value={{ isDarkMode, toggleDarkMode }}>
+      {children}
+    </DarkModeContext.Provider>
+  )
+}
+
+function useDarkMode() {
+  const context = useContext(DarkModeContext)
+
+  if (context === undefined)
+    throw new Error('DarkModeContext was used outside of DarkModeProvider')
+
+  return context
+}
+
+export { DarkModeProvider, useDarkMode }
