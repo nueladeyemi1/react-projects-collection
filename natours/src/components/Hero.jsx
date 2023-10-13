49610948b1200bdbe0f92d1f5522893fc3a9c2b9
nueladeyemi1 <emmanuel.adeyemi40@gmail.com>
2023-10-13 19:44:54 +0100

    sass

--- natours/src/components/Hero.jsx
+++ natours/src/components/Hero.jsx
@@ -1,5 +1,22 @@
+import { Link } from 'react-router-dom'
+
 const Hero = () => {
-  return <header className='header'>Some text...</header>
+  return (
+    <header className='header'>
+      <div className='header-logo-box'>
+        <img className='header-logo' src='./logo-white.png' alt='Logo' />
+      </div>
+      <div className='header-text-box'>
+        <h1 className='primary-heading'>
+          <span className='primary-heading-main'>Outdoors</span>
+          <span className='primary-heading-sub'>is where life happens</span>
+        </h1>
+        <a className='btn btn-white btn-animated' href='#'>
+          Discover our tours
+        </a>
+      </div>
+    </header>
+  )
 }
 
 export default Hero
