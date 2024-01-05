b1bf656ee43480b6686fc249c090e8a4c893bda2
nueladeyemi1 <emmanuel.adeyemi40@gmail.com>
2024-01-05 18:11:34 +0100

    thecheesecake --all-done

--- cheesecake/src/components/Header.jsx
+++ cheesecake/src/components/Header.jsx
@@ -11,15 +11,21 @@ const Header = () => {
       <img
         src='LOGO OF CHEESECAKE.png'
         alt='cheesecake logo'
-        className='header-logo'
+        className={toggle ? 'logo-fix header-logo' : 'header-logo'}
       />
-      <div onClick={() => setToggle((state) => !state)} className='mobile-nav'>
+      <div
+        onClick={() => setToggle((state) => !state)}
+        className={toggle ? 'fix mobile-nav' : 'mobile-nav'}
+      >
         {toggle ? (
           <RxCross1 size={35} color='#6A4028' />
         ) : (
           <CiMenuFries size={35} color='#6A4028' />
         )}
       </div>
+      <div
+        className={toggle ? 'mobile-background' : 'no-mobile-background'}
+      ></div>
       <NavLinks toggle={toggle} />
       <button className={toggle ? 'show-mobile' : 'header-signup'}>
         Sign up
